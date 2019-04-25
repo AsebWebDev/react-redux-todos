59e8ea78a6c3d922a8ef18af6c9fccc8e7982a6b
André Sebastian <a.sebastian@posteo.de>
2019-04-25 15:25:50 +0200

    add thunk middleware via compose

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./rootreducer";
 import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose (
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
